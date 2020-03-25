d118eac4e66c06875cdf490ebf1174460faa6a34
Stephen <stephen.c.weiss@gmail.com>
2020-03-25 12:48:42 -0500

    refactor: styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react'
+import { Rappers } from './components'
 
-const App = () => <>SupaHotFire</>
+const App = () => (
+  <>
+    <h1>SupaHotFire!!</h1>
+    <Rappers />
+  </>
+)
 
 export default App
